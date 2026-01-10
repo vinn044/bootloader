c5718db7e68ffa815904837f15e2de966e29e496
vinn044 <vincek1800@gmail.com>
2026-01-10 05:54:56 -0500

    bootloader fail

--- bootloader.asm
+++ bootloader.asm
@@ -1,7 +1,161 @@
-mov ax, 0x7C0   ; data segment points to where bootloader  was loaded
+bits 16
+org 0x7C00
+
+mov ax, 0x07C0
 mov ds, ax
-
-mov ax, 0x7E0
+mov ax, 0x07E0      ; 07E0h = (07C00h+200h)/10h, beginning of stack segment.
 mov ss, ax
+mov sp, 0x2000      ; 8k of stack space.
 
-mov sp, 0x2000
+call clearscreen
+
+push 0x0000
+call movecursor
+add sp, 2
+
+push msg
+call print
+add sp, 2
+
+cli
+hlt
+
+clearscreen:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov ah, 0x07        ; tells BIOS to scroll down window
+    mov al, 0x00        ; clear entire window
+    mov bh, 0x07        ; white on black
+    mov cx, 0x00        ; specifies top left of screen as (0,0)
+    mov dh, 0x18        ; 18h = 24 rows of chars
+    mov dl, 0x4f        ; 4fh = 79 cols of chars
+    int 0x10        ; calls video interrupt
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+movecursor:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov dx, [bp+4]      ; get the argument from the stack. |bp| = 2, |arg| = 2
+    mov ah, 0x02        ; set cursor position
+    mov bh, 0x00        ; page 0 - doesn't matter, we're not using double-buffering
+    int 0x10
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+print:
+    push bp
+    mov bp, sp
+    pusha
+    mov si, [bp+4]      ; grab the pointer to the data
+    mov bh, 0x00        ; page number, 0 again
+    mov bl, 0x00        ; foreground color, irrelevant - in text mode
+    mov ah, 0x0E        ; print character to TTY
+.char:
+    mov al, [si]        ; get the current char from our pointer position
+    add si, 1       ; keep incrementing si until we see a null char
+    or al, 0
+    je .return          ; end if the string is done
+    int 0x10            ; print the character if we're not done
+    jmp .char       ; keep looping
+.return:
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+
+msg:    db "this is my bootloader nigga", 0
+
+times 510-($-$$) db 0
+dw 0xAA55
+bits 16
+org 0x7C00
+
+xor ax, ax
+mov ds, ax
+mov ax, 0x07E0      ; 07E0h = (07C00h+200h)/10h, beginning of stack segment.
+mov ss, ax
+mov sp, 0x2000      ; 8k of stack space.
+
+call clearscreen
+
+push 0x0000
+call movecursor
+add sp, 2
+
+push msg
+call print
+add sp, 2
+
+cli
+hlt
+
+clearscreen:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov ah, 0x07
+    mov al, 0x00
+    mov bh, 0x07
+    mov cx, 0x00
+    mov dh, 0x18
+    mov dl, 0x4f
+    int 0x10
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+movecursor:
+    push bp
+    mov bp, sp
+    pusha
+
+    mov dx, [bp+4]
+    mov ah, 0x02
+    mov bh, 0x00
+    int 0x10
+
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+print:
+    push bp
+    mov bp, sp
+    pusha
+    mov si, [bp+4]
+    mov bh, 0x00
+    mov bl, 0x00
+    mov ah, 0x0E
+.char:
+    mov al, [si]
+    add si, 1
+    or al, 0
+    je .return
+    int 0x10
+    jmp .char
+.return:
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+msg: db "this is my bootloader", 0
+
+times 510-($-$$) db 0
+dw 0xAA55
