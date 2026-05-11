e3f782db04da88057209c4347523f504c8371278
vin <vincek1800@gmail.com>
2026-05-10 19:47:25 -0500

    rewipe, starting simple.

--- bootloader.asm
+++ bootloader.asm
@@ -1,81 +1,20 @@
 bits 16
-org 0x7C00
+org 0x7c00
 
-mov ax, 0x07C0
-mov ds, ax
-mov ax, 0x07E0      ; 07E0h = (07C00h+200h)/10h, beginning of stack segment.
-mov ss, ax
-mov sp, 0x2000      ; 8k of stack space.
-
-call clearscreen
-
-push 0x0000
-call movecursor
-add sp, 2
-
-push msg
-call print
-add sp, 2
-
-cli
-hlt
-
-clearscreen:
-    push bp
-    mov bp, sp
-    pusha
-
-    mov ah, 0x07        ; tells BIOS to scroll down window
-    mov al, 0x00        ; clear entire window
-    mov bh, 0x07        ; white on black
-    mov cx, 0x00        ; specifies top left of screen as (0,0)
-    mov dh, 0x18        ; 18h = 24 rows of chars
-    mov dl, 0x4f        ; 4fh = 79 cols of chars
-    int 0x10        ; calls video interrupt
-
-    popa
-    mov sp, bp
-    pop bp
-    ret
-
-movecursor:
-    push bp
-    mov bp, sp
-    pusha
-
-    mov dx, [bp+4]      ; get the argument from the stack. |bp| = 2, |arg| = 2
-    mov ah, 0x02        ; set cursor position
-    mov bh, 0x00        ; page 0 - doesn't matter, we're not using double-buffering
-    int 0x10
-
-    popa
-    mov sp, bp
-    pop bp
-    ret
+mov si, 0
 
 print:
-    push bp
-    mov bp, sp
-    pusha
-    mov si, [bp+4]      ; grab the pointer to the data
-    mov bh, 0x00        ; page number, 0 again
-    mov bl, 0x00        ; foreground color, irrelevant - in text mode
-    mov ah, 0x0E        ; print character to TTY
-.char:
-    mov al, [si]        ; get the current char from our pointer position
-    add si, 1       ; keep incrementing si until we see a null char
-    or al, 0
-    je .return          ; end if the string is done
-    int 0x10            ; print the character if we're not done
-    jmp .char       ; keep looping
-.return:
-    popa
-    mov sp, bp
-    pop bp
-    ret
+    mov ah, 0x0e
+    mov al, [hello + si]
+    int 0x10
+    add si, 1
+    cmp byte [hello + si], 0
+    jne print
 
+jmp $
 
-msg:    db "this is my bootloader", 0
+hello:
+    db "Hello every1!", 0
 
-times 510-($-$$) db 0
+times  510 - ($ - $$) db 0
 dw 0xAA55
